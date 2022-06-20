c3d8ea28da938af23d175b134526b74630b0f5e2
Karl Lundqvist <karl.lundqvist0@gmail.com>
2022-06-20 13:16:23 +0200

    Add capitalize and oneRound functions

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-
 function computerPlay(array) {
     const randomIndex = Math.floor(Math.random() * array.length);
     const item = array[randomIndex];
@@ -9,6 +7,39 @@ function computerPlay(array) {
 
 const choices = ['Rock', 'Paper', 'Scissors'];
 const result = computerPlay(choices);
-console.log(`computer plays ${result}`);
+
+function capitalize(str){
+    const lower = str.toLowerCase(str);
+    return str.charAt(0).toUpperCase() + lower.slice(1);
+}
+
+function oneRound(computerSelection, playerSelection) {
+    const rock = 'Rock'
+    const paper = 'Paper'
+    const scissors = 'Scissors'
 
 
+        if (computerSelection === playerSelection) {
+            console.log('Draw');
+        }
+        else if (computerSelection === paper && playerSelection === rock){ 
+            console.log(`You Lose! Computer played ${computerSelection} and you played ${playerSelection}`);
+        }
+        else if (computerSelection === rock && playerSelection === scissors){
+            console.log(`You Lose! Computer played ${computerSelection} and you played ${playerSelection}`);
+        }
+        else if (computerSelection === scissors && playerSelection === paper){
+            console.log(`You Lose! Computer played ${computerSelection} and you played ${playerSelection}`);
+        }
+        else{
+            console.log(`You won! You played ${playerSelection} and computer played ${computerSelection}`)
+    }
+}
+
+const computerSelection = result;
+const playerSelection = capitalize('rOck');
+
+console.log(result);
+console.log(playerSelection);
+
+oneRound(computerSelection, playerSelection)
