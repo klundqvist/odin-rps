5bf17213f4194a2ba445b92de65074bd660f41fc
Karl Lundqvist <karl.lundqvist0@gmail.com>
2022-06-20 13:28:16 +0200

    Change console.log to return

--- script.js
+++ script.js
@@ -19,23 +19,23 @@ function oneRound(computerSelection, playerSelection) {
 
 
         if (computerSelection === playerSelection) {
-            console.log(`Draw. You played ${computerSelection} and computer played ${playerSelection}`);
+            return `Draw. You played ${computerSelection} and computer played ${playerSelection}`;
         }
         else if (computerSelection === paper && playerSelection === rock){ 
-            console.log(`You Lose! Computer played ${computerSelection} and you played ${playerSelection}`);
+            return `You Lose! Computer played ${computerSelection} and you played ${playerSelection}`;
         }
         else if (computerSelection === rock && playerSelection === scissors){
-            console.log(`You Lose! Computer played ${computerSelection} and you played ${playerSelection}`);
+            return `You Lose! Computer played ${computerSelection} and you played ${playerSelection}`;
         }
         else if (computerSelection === scissors && playerSelection === paper){
-            console.log(`You Lose! Computer played ${computerSelection} and you played ${playerSelection}`);
+            return `You Lose! Computer played ${computerSelection} and you played ${playerSelection}`;
         }
         else{
-            console.log(`You won! You played ${playerSelection} and computer played ${computerSelection}`)
+            return `You won! You played ${playerSelection} and computer played ${computerSelection}`
     }
 }
 
 const computerSelection = computerPlay(choices);
 const playerSelection = capitalize('rOck');
 
-oneRound(computerSelection, playerSelection)
+console.log(oneRound(computerSelection, playerSelection));
