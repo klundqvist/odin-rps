e99f1b498c09f663f7758be9bc31c65c7d6e4f2b
Karl Lundqvist <karl.lundqvist0@gmail.com>
2022-06-20 17:39:39 +0200

    Add game() function

--- script.js
+++ script.js
@@ -1,21 +1,10 @@
-function computerPlay(array) {
-    const randomIndex = Math.floor(Math.random() * array.length);
-    const item = array[randomIndex];
-    return item;
-    
-}
 
-const choices = ['Rock', 'Paper', 'Scissors'];
-
-function capitalize(str){
-    const lower = str.toLowerCase(str);
-    return str.charAt(0).toUpperCase() + lower.slice(1);
-}
-
+/* OLD oneRound()
 function oneRound(computerSelection, playerSelection) {
     const rock = 'Rock'
     const paper = 'Paper'
     const scissors = 'Scissors'
+    
 
 
         if (computerSelection === playerSelection) {
@@ -34,8 +23,80 @@ function oneRound(computerSelection, playerSelection) {
             return `You won! You played ${playerSelection} and computer played ${computerSelection}`
     }
 }
+*/
+function computerPlay(array) {
+    const randomIndex = Math.floor(Math.random() * array.length);
+    const item = array[randomIndex];
+    return item;
+    
+}
 
-const computerSelection = computerPlay(choices);
-const playerSelection = capitalize('rOck');
+const choices = ['Rock', 'Paper', 'Scissors'];
 
-console.log(oneRound(computerSelection, playerSelection));
+function capitalize(str){
+    const lower = str.toLowerCase(str);
+    return str.charAt(0).toUpperCase() + lower.slice(1);
+}
+
+const playerSelection = capitalize(window.prompt('Write Rock, Paper or Scissors!'));
+
+var playerScore = 0;
+var computerScore = 0;
+var drawScore = 0;
+
+function oneRound(computerSelection, playerSelection) {
+    const rock = 'Rock'
+    const paper = 'Paper'
+    const scissors = 'Scissors'
+
+    
+
+
+        if (computerSelection === playerSelection) {
+            drawScore++;
+        }
+        else if (computerSelection === paper && playerSelection === rock){ 
+            computerScore++;
+        }
+        else if (computerSelection === rock && playerSelection === scissors){
+            computerScore++;
+        }
+        else if (computerSelection === scissors && playerSelection === paper){
+            computerScore++;
+        }
+        else{
+            playerScore++;
+    }
+}
+
+function game(){
+    if (!(choices.includes(playerSelection))) {
+        alert(`Hey! That word is not allowed`);
+        location.reload();
+        }
+    else {
+        for (let i = 0; i < 5; i++){
+            
+            const computerSelection = computerPlay(choices);
+            oneRound(computerSelection, playerSelection);
+            console.log(playerSelection, computerSelection);
+            console.log(playerScore, computerScore, drawScore);
+        }
+
+        
+
+        if (playerScore === computerScore) {
+            return `It's a draw. Computer got ${computerScore} points, you got ${playerScore} points and you played ${drawScore} equal games.`
+        }
+        else if (playerScore < computerScore) {
+            return `You lost! Computer got ${computerScore} points, you got ${playerScore} points and you played ${drawScore} equal games.`
+        }
+        else{
+            return `You won! Computer got ${computerScore} points, you got ${playerScore} points and you played ${drawScore} equal games.`
+        }
+
+        
+}}
+
+console.log(playerSelection)
+console.log(game())
